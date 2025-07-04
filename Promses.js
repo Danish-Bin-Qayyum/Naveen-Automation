21dcffe16a6f7130c20246152403ce3ffe91285d
Danish Qayyum <danishbinqayyum123@gmail.com>
2025-07-04 02:30:05 -0700

    second committ

--- Promses.js
+++ Promses.js
@@ -24,7 +24,7 @@ randonValuePromisis // call the promise  ( related calling ? )
     console.log("promosis is fulfilled ", result); //print the resolve statement
 })
 .catch(Error => {
-   console.error("promisis is reject" , Error);
+   console.error("promisis is reject" , Error);// catch the error froam promise in case of reject promise
 });
 //      output is fulfillled
 
@@ -52,35 +52,36 @@ getevennumbe(2,1000) // calling a normal function passing arrguments
     console.log("step 1 get the result even number",result);//print the resolve statement step 1
     return getevennumber(3,2000) // calling here normal function
 })                               // it will call to next .then() method
-.then(result=>{
+.then(result=>{ // .then() method in JavaScript , which are objects representing the eventual completion or failure of an asynchronous operation.
  console.log("step 1 get the result even number",result);//print the resolve statement step 2
 })
 .catch(error=>{
-    console.log("promisis chain error",error);//print the error statement 
+    console.log("promisis chain error",error);// catch the error froam promise in case of reject promise
 })
 
 ///===>Promises.all
 // -->it use when want you to perform multiple asynic operatins in parallel
 // --> wait for all of them to complete before moving on ( when all the promise in completed then give anser in form arry )
-const function1 = ()=>{
-    return new Promise ((resolve, reject)=> {
-        setInterval(() => {
-            resolve("don")
-        }, 2000);
+// --> it will not run if any promise is rejeect
+const function1 = ()=>{// arrow function
+    return new Promise ((resolve, reject)=> { // promise function define 1
+        setInterval(() => {//  schedule the execution of a function or a piece of code after a specified delay
+            resolve("don")//  constructor's executor function to manage the promise's state resole value 
+        }, 2000); // delay time
     })
 }
 const function2 = ()=>{
-    return new Promise ((resolve, reject)=> {
-        setInterval(() => {
-            resolve("don")
-        }, 2000);
+    return new Promise ((resolve, reject)=> {// promise function define 2
+        setInterval(() => {//  schedule the execution of a function or a piece of code after a specified delay
+            resolve("don")//  constructor's executor function to manage the promise's state resole value 
+        }, 2000); // delay time
     })
 }
 const function3 = ()=>{
-    return new Promise ((resolve, reject)=> {
-        setInterval(() => {
-            resolve("don")
-        }, 2000);
+    return new Promise ((resolve, reject)=> {// promise function define 3
+        setInterval(() => {//  schedule the execution of a function or a piece of code after a specified delay
+            resolve("don")//  constructor's executor function to manage the promise's state resole value 
+        }, 2000); // delay time
     })
 }
 Promise.all([       // give result of all promise in array 
@@ -88,73 +89,78 @@ Promise.all([       // give result of all promise in array
     function2(),    // usin in Fetching Multiple Resources Simultaneously
     function3(),    //Parallel Processing of Independent Tasks:
 ])
- .then(dataArry => {
+ .then(dataArry => { // .then() method in JavaScript , which are objects representing the eventual completion or failure of an asynchronous operation.
     console.log('all data from fun',dataArry);//print the resolve statement
  })
  .catch(error => {
-    console.log('error is promisis');//print the error statement 
+    console.log('error is promisis');// catch the error froam promise in case of reject promise
  })
  /// ==> output in (all data from fun [ 'don','don', 'don'])
 
 ///===> Promises.Allsettled
 // --> if the number of promises is resolve it will give you a combin result
 // --> usful when you want to process, whether they succeed or fail,
+// --> it use when want you to perform multiple asynic operatins in parallel
 // --> you want to gather information about the outcome of each promises 
-/*const getData = ()=>{
+// --> it will be run if any of promise if reject and give other promises
+const getData = ()=>{
     return new Promise ((resolve, reject)=> {
-        setInterval(() => {
-            resolve("data from fetdat")
-        }, 2000);
+        setInterval(() => {//  schedule the execution of a function or a piece of code after a specified delay
+            resolve("data from fetdat")//  constructor's executor function to manage the promise's state resole value 
+        }, 2000); // delay time
     })
 }
 const getError = ()=>{
     return new Promise ((resolve, reject)=> {
-        setInterval(() => {
-            resolve("error : data is not avaible from geterror")
-        }, 2000);
+        setInterval(() => {//  schedule the execution of a function or a piece of code after a specified delay
+            resolve("error : data is not avaible from geterror")//  constructor's executor function to manage the promise's state resole value 
+        }, 2000); // delay time
     })
 }
-Promise.allSettled([
+Promise.allSettled([  // it wiil settled all promise if any of them is rejected
     getData(),
     getError()
 ])
-.then(results =>{
+.then(results =>{ // .then() method in JavaScript , which are objects representing the eventual completion or failure of an asynchronous operation.
     results.forEach(results=>{
-        if (results.status === "fulfilled") {
+        if (results.status === "fulfilled") { // status keyword is use to check th status of promise 
             console.log("value",results.value);
         } else {
-           console.error("reason for rejection ",results.reason);
+           console.error("reason for rejection ",results.reason);//( reason key word ?)
            
         }
     })
-})
+})// give us both uotput resolve reject 
+
+
 
 // Promises
 // --> to see the which promise settles first
 const resolvePromisis = ()=>{
     return new Promise ((resolve, reject)=> {
-        setInterval(() => {
-            resolve("resolve")
-        }, 1000);
+        setInterval(() => {//  schedule the execution of a function or a piece of code after a specified delay
+            resolve("resolve")//  constructor's executor function to manage the promise's state resole value 
+        }, 1000); // delay time
     })
 }
 const rejectPromisis = ()=>{
     return new Promise ((resolve, reject)=> {
-        setInterval(() => {
-            resolve("rejected")
-        }, 500);
+        setInterval(() => {//  schedule the execution of a function or a piece of code after a specified delay
+            resolve("rejected")//  constructor's executor function to manage the promise's state resole value 
+        }, 500); // delay time
     })
 }
 Promise.race([
     resolvePromisis(),
     rejectPromisis()
 ])
-.then(result =>{
+.then(result =>{ // .then() method in JavaScript , which are objects representing the eventual completion or failure of an asynchronous operation.
     console.log("result", result);
 })
 .catch(error =>{
-    console.log("error" ,error);
+    console.log("error" ,error);// catch the error froam promise in case of reject promise
 })
+//output is rejected becouse second method has less time
 
 ///==>Promises.any
 // --> it is used for handlin multiple promises and resolving with value of the first 
@@ -163,39 +169,41 @@ Promise.race([
 //   promise fulfills/resolved
 const getmsg = ()=>{
     return new Promise ((resolve, reject)=> {
-        setInterval(() => {
-            resolve("get msg from api")
-        }, 500);
+        setInterval(() => {//  schedule the execution of a function or a piece of code after a specified delay
+            resolve("get msg from api")//  constructor's executor function to manage the promise's state resole value 
+        }, 500); // delay time
     })
 }
 const getuser = ()=>{
     return new Promise ((resolve, reject)=> {
-        setInterval(() => {
-            resolve("error  user dat could not be fetched")
-        }, 200);
+        setInterval(() => {//  schedule the execution of a function or a piece of code after a specified delay
+            resolve("error  user dat could not be fetched")//  constructor's executor function to manage the promise's state resole value 
+        }, 200); // delay time
     })
 }
 const getProduct = ()=>{
     return new Promise ((resolve, reject)=> {
-        setInterval(() => {
-            resolve("product could not be fetched")
-        }, 1000);
+        setInterval(() => {//  schedule the execution of a function or a piece of code after a specified delay
+            resolve("product could not be fetched")//  constructor's executor function to manage the promise's state resole value 
+        }, 1000); // delay time
     })
 }
-Promise.any([
+Promise.any([ // it is wait for promise to any of them is resolve if no any reslve then it will give a error
     getmsg(),
     getuser(),
     getmsg(),
 ])
-.then(results =>{
+.then(results =>{ // .then() method in JavaScript , which are objects representing the eventual completion or failure of an asynchronous operation.
     console.log('first succesful result ', results);//will log the first resolve promise
 })
 .catch(error => {
-    console.error("all promise rejected",error);//will log all rejection reasons reject
-})*/
+    console.error("all promise rejected",error);// catch the error froam promise in case of reject promise
+})
+ // out put is first method is reslove
 
 
 // -----------------------------------( Summar )----------------------------------------///
+//
 // promise class give six method to handling API
 // there are six methods to handling  async parallel programming 
 // .all , .any, .allsettled and race which are perfoam task based on array
@@ -205,10 +213,6 @@ Promise.any([
 
 
 
-
-
-
-
-
+ 
 
 

97fcf27294a9516b28013e5889ba96eaa6e53b75
unknown <danishbinqayyum123@gmail.com>
2025-07-12 10:52:25 +0500

    11/7/2025

--- JavaSript.js
+++ JavaSript.js
@@ -1,15 +1,3 @@
 ///--------------------------------( importan infor mation about   javascript ) -------//
- //--> it is an case sencetive language 
-
-<<<<<<< HEAD
- function test() {
-  if (true) {
-    var x = 1;
-    let y = 2;
-  }
-  console.log(x); 
-  console.log(y); 
-}
-=======
-
->>>>>>> 358123c1352c9f5162ceb24c55d07a9046ecc5ce
+ //--> it is an case sencetive language
+ 
